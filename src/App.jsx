6123f174efe3aafb01f2db92b975cffdaae8e6e5
TomasWielandt <twielandt16@gmail.com>
2024-08-14 17:04:58 -0400

    se crea el componente ErrorBoundary.jsx en la carpeta components y se monta la aplicacion en APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
 
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <ErrorBoundary>
+        <Navbar />
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </ErrorBoundary>
     </Router>
   )
 }
