b8a0bb7cc95783b2c74801a318b4fc1e0fd647e7
TomasWielandt <twielandt16@gmail.com>
2024-08-14 20:00:39 -0400

    se mejoro la interfaz de PokemonCard, nombre en mayuscula y color amarillo, borde negro y sombra, se agrando un poco la imagen y al hacer hover se agranda un poco mas

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,9 +4,9 @@ const PokemonCard = ({ pokemon }) => {
   if (!pokemon) return null;
 
   return (
-    <div className="bg-gray-800 text-white p-4 rounded-lg flex flex-col items-center">
-      <h2 className="text-2xl font-bold mb-2 text-transform: uppercase">{pokemon.name}</h2>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} className="mb-2 w-36" />
+    <div className="bg-gray-800 text-white p-4 rounded-lg flex flex-col items-center border-4 border-black shadow-black shadow-lg">
+      <h2 className="text-2xl font-bold mb-2 text-transform: uppercase text-yellow-400">{pokemon.name}</h2>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} className="mb-2 w-36 transform transition-transform duration-300 hover:scale-110" />
       <p className="mb-2">Height: {pokemon.height}</p>
       <p className="mb-2">Weight: {pokemon.weight}</p>
       <p className="mb-2">Base Experience: {pokemon.base_experience}</p>
